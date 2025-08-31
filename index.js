73d9c8933e538b881827963ffc4afe1f0db433ef
Azhar code <112419808+Azhar1277@users.noreply.github.com>
2025-08-31 23:15:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,164 @@
+var swiper = new Swiper(".small-container", {
+
+  spaceBetween:20,
+  //loop:true,
+
+ freeMode: true,
+
+ //speed:1000,
+
+ // pagination: {
+  //  el: ".swiper-pagination",
+   // clickable: true,
+ // },
+  //centeredSlides:true,
+
+
+  breakpoints: {
+    0: {
+      slidesPerView:0,
+      
+    },
+    568: {
+      slidesPerView: 3,
+      
+    },
+    768: {
+      slidesPerView: 4,
+    
+    },
+    968: {
+      slidesPerView:5,
+      
+    },
+   
+  },
+  keyboard:true,
+ // mousewheel:true,
+
+
+
+
+
+  //navigation: {
+    //nextEl: ".swiper-button-next",
+   // prevEl: ".swiper-button-prev",
+ // },
+
+});
+var swiper = new Swiper(".small1-container", {
+
+  spaceBetween:20,
+  //loop:true,
+
+ freeMode: true,
+
+ //speed:1000,
+  // autoplay: {
+   //   delay: 5500,
+   //   disableOnInteraction: false,
+    //},
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  //centeredSlides:true,
+
+
+  breakpoints: {
+    0: {
+      slidesPerView:0,
+      
+    },
+    568: {
+      slidesPerView: 3,
+      
+    },
+    768: {
+      slidesPerView: 4,
+    
+    },
+    968: {
+      slidesPerView:5,
+      
+    },
+   
+  },
+  keyboard:true,
+ // mousewheel:true,
+
+
+
+
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+  },
+
+});
+
+
+
+//small2
+
+var swiper = new Swiper(".small2-container", {
+
+  spaceBetween:20,
+  //loop:true,
+
+ freeMode: true,
+
+ //speed:1000,
+   autoplay: {
+     delay: 2500,
+   disableOnInteraction: false,
+    },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  //centeredSlides:true,
+
+
+  breakpoints: {
+    0: {
+      slidesPerView:0,
+      
+    },
+    568: {
+      slidesPerView: 3,
+      
+    },
+    768: {
+      slidesPerView: 3,
+    
+    },
+    968: {
+      slidesPerView:3,
+      
+    },
+   
+  },
+  keyboard:true,
+ // mousewheel:true,
+
+
+
+
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+  },
+
+});
+
+
+
+
+
+
+
